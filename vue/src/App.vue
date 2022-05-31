9abb7fa7714e95796397d5c28c652b020f3d3a96
Drew Reed <dreed@spherexx.com>
2022-05-31 13:25:28 -0500

    Pushing longin frontend

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,35 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'home' }" class="link">Home</router-link>
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''" class="link" &nbsp;|&nbsp;>Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<style scoped>
+
+  #nav {
+    background-color: #3c99dc;
+    padding: 5px;
+  }
+
+  .link {
+    text-decoration: none;
+    color: black;
+    background-color: #3c99dc;
+    font-size: 1.4rem;
+    padding: 5px;
+    transition: .2s;
+  }
+  
+  .link:hover {
+    transform: scale(1.5);
+}
+
+  .link:active {
+    color: white;
+  }
+
+</style>
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="login" class="text-center">
     <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
+      <h1 class="h3 mb-3 font-weight-normal">Features, Not Bugs</h1>
       <div
         class="alert alert-danger"
         role="alert"
@@ -12,7 +12,7 @@
         role="alert"
         v-if="this.$route.query.registration"
       >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
+      <!-- <label for="username" class="sr-only">Username</label> -->
       <input
         type="text"
         id="username"
@@ -22,7 +22,7 @@
         required
         autofocus
       />
-      <label for="password" class="sr-only">Password</label>
+      <!-- <label for="password" class="sr-only">Password</label> -->
       <input
         type="password"
         id="password"
@@ -31,9 +31,9 @@
         v-model="user.password"
         required
       />
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
       <button type="submit">Sign in</button>
     </form>
+    <router-link :to="{ name: 'register' }" class="newAccount">Need an account?</router-link>
   </div>
 </template>
 
@@ -74,3 +74,101 @@ export default {
   }
 };
 </script>
+
+<style>
+
+* {
+  font-family: Arial, Helvetica, sans-serif;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  background-color: #d5f3fe;
+}
+
+#login{
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  height: auto;
+  color: #313131;
+  font-size: 1.5rem;;
+  transform: translate(-50%, -50%);
+  padding: 30px;
+  appearance: none;
+  border:none;
+  outline: none;
+  background: none;
+  border-radius: 5px;
+  box-shadow: 0px 0px 8px rgba(0, 175, 240, 0.5);
+  /* background-color: #3c99dc; */
+}
+
+#login  h1 {
+  display: flex;
+  justify-content: center;
+  padding: 25px 0 25px 25px;
+  font-size: 2rem;
+}
+
+#login label {
+  display: flex;
+  justify-content: center;
+  padding: 5px;
+}
+
+#username, #password {
+  display: flex;
+  justify-content: center;
+  border-radius: 5px;
+  box-shadow: 0px 0px 8px rgba(0, 175, 240, 0.999525);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 3px;
+  font-size: 1.8rem;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 15px;
+  cursor: text;
+  padding-left: 5px;
+  width: auto;
+}
+
+#username:focus{
+  background: #00AFF0;
+}
+#password:focus{
+  background: #00AFF0
+}
+
+form button {
+  background-color: #00AFF0;
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 15px;
+  font-size: 18px;
+  padding: 3px;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+.newAccount {
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  padding-top: 25px;
+  color: #000000;
+  }
+
+.newAccount:hover {
+  color: #00AFF0;
+}
+
+.alert-danger {
+  color: black;
+  font-weight: bolder;
+  display: flex;
+  justify-content: center;
+}
+
+</style>
