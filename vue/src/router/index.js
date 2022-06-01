4e2944246931ee5f75feb923962d714c8cccbaab
Colin McGregor <colin.w.mcgregor@gmail.com>
2022-06-01 13:02:32 -0500

    starting to make userlist view

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../views/Login.vue'
 import Logout from '../views/Logout.vue'
 import Register from '../views/Register.vue'
 import store from '../store/index'
+import ListUsers from '../views/ListUsers.vue'
 
 Vue.use(Router)
 
@@ -53,6 +54,14 @@ const router = new Router({
         requiresAuth: false
       }
     },
+    {
+      path: "/userlist",
+      name: "userlist",
+      component: ListUsers,
+      meta: {
+        requiresAuth: false
+      }
+    }
   ]
 })
 
---
+++ vue/src/views/ListUsers.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='userList'>
+      <h1>List of Users</h1>
+    <users-list/>
+  </div>
+</template>
+
+<script>
+import UsersList from '../components/UsersList.vue';
+export default {
+name: 'ListOfUsers',
+components:{
+    UsersList
+}
+}
+</script>
+
+<style>
+
+</style>
