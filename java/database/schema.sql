d51866fe3331fec79a5a920203357362b67136fb
Jack Khodr <jakhodr@gmail.com>
2022-06-02 14:09:07 -0700

    fixed database

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -18,24 +18,19 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-CREATE TABLE visitors (
-	visitor_id int NOT NULL,
-	firstName varchar(50),
-	lastName varchar(50),
-	company_position varchar(25),
-	CONSTRAINT PK_visitor PRIMARY KEY (visitor_id)
-);
-
-CREATE TABLE managers (
-	manager_id int NOT NULL,
+CREATE TABLE contacts (
+	contact_id int NOT NULL,
 	firstName varchar(50) NOT NULL,
 	lastName varchar(50) NOT NULL,
 	phoneNumber varchar(25) NOT NULL,
 	email varchar(50) NOT NULL,
-	municipality varchar(50) NOT NULL,
-	manager_role varchar(50) NOT NULL,
-	CONSTRAINT PK_manager PRIMARY KEY (manager_id),
-	CONSTRAINT FK_manager_user FOREIGN KEY (manager_id) REFERENCES users(user_id)
+	municipality varchar(50),
+	contact_role varchar(50) NOT NULL,
+	companyName varchar(50),
+	industry varchar(50),
+	contractor_address varchar(100),
+	CONSTRAINT PK_contact PRIMARY KEY (contact_id),
+	CONSTRAINT FK_contact_user FOREIGN KEY (contact_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE groups (
@@ -43,7 +38,7 @@ CREATE TABLE groups (
 	group_name varchar(50) NOT NULL,
 	program_manager_id int NOT NULL,
 	CONSTRAINT PK_group PRIMARY KEY (group_id),
-	CONSTRAINT FK_group_manager FOREIGN KEY (program_manager_id) REFERENCES managers(manager_id)
+	CONSTRAINT FK_group_manager FOREIGN KEY (program_manager_id) REFERENCES contacts(contact_id)
 );
 
 CREATE TABLE projects (
@@ -55,29 +50,18 @@ CREATE TABLE projects (
 	municipality varchar(50) NOT NULL,
 	project_manager_id int NOT NULL,
 	group_id int NOT NULL,
+	contractName varchar(50),
+	fundingSource varchar (50),
 	CONSTRAINT PK_project PRIMARY KEY (project_id),
-	CONSTRAINT FK_project_manager FOREIGN KEY (project_manager_id) REFERENCES managers(manager_id),
+	CONSTRAINT FK_project_manager FOREIGN KEY (project_manager_id) REFERENCES contacts(contact_id),
 	CONSTRAINT FK_group_projects FOREIGN KEY (group_id) REFERENCES groups(group_id)
-);
-
-CREATE TABLE contractors (
-	contractor_id int NOT NULL,
-	firstName varchar(50) NOT NULL,
-	lastName varchar(50) NOT NULL,
-	phoneNumber varchar(25) NOT NULL,
-	email varchar(50) NOT NULL,
-	companyName varchar(50) NOT NULL,
-	industry varchar(25) NOT NULL,
-	contractor_address varchar(100) NOT NULL,
-	CONSTRAINT PK_contractor PRIMARY KEY (contractor_id),
-	CONSTRAINT FK_contractor_user FOREIGN KEY (contractor_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE project_contractors (
 	contractor_id int NOT NULL,
 	project_id varchar(50) NOT NULL,
 	CONSTRAINT PK_project_contractor PRIMARY KEY (contractor_id, project_id),
-	CONSTRAINT FK_project_contractor_contractors FOREIGN KEY (contractor_id) REFERENCES contractors(contractor_id),
+	CONSTRAINT FK_project_contractor_contractors FOREIGN KEY (contractor_id) REFERENCES contacts(contact_id),
 	CONSTRAINT FK_project_contractor_project FOREIGN KEY (project_id) REFERENCES projects(project_id)
 );
 
