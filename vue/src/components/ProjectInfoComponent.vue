8831709fb9a8b08e7f1c664b0755499b66068201
Colin McGregor <colin.w.mcgregor@gmail.com>
2022-06-02 11:12:55 -0500

    getting lists to display info

--- vue/src/components/ProjectInfoComponent.vue
+++ vue/src/components/ProjectInfoComponent.vue
@@ -13,17 +13,31 @@
 </template>
 
 <script>
-import ProjectService from '../services/ProjectService'
+ import ProjectService from '../services/ProjectService'
 export default {
-name:'project-info-component',
-props:{
-    'projectId':Number
-},
-data(){
-    project:{
-        projectId:0
+ name:'project-info-component',
+ props:{
+     'projectId':Number
+ },
+ data(){
+     return{
+     project:{
+         projectId:0,
+         project_name:'',
+        project_description:'',
+        project_location:'',
+        precinct:'',
+        municipality:'',
+        project_manager_id:0,
+        group_id:0
     }
-}
+     }
+ },
+ created(){
+     ProjectService.getProjectById(this.$route.params.id).then(response=>{
+         this.project=response.data;
+     })
+ }
 }
 </script>
 
